b17ca4e18db425a3ec1363d9fa2020e8cae24d46
DheerajShriyan <dheerajshriyan333@gmail.com>
2024-06-18 15:53:36 +0530

    added a new module

--- src/components/card.js
+++ src/components/card.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 const Card = ({ title, description, link, photo }) => {
   return (
-    <div className="card">
+    <div className="card"><a href={link}>
       <img src={photo} alt={title} />
+      
       <div className="card-body">
         <h3>{title}</h3>
         <p>{description}</p>
         <a href={link}>Learn More</a>
-      </div>
+      </div></a>
     </div>
   );
 };
